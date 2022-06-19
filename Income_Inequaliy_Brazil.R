70e83c81ef075ab168f8515b2b59bc1c1606d07b
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-06-19 17:31:11 +0200

    Update Income_Inequaliy_Brazil.R

--- Income_Inequaliy_Brazil.R
+++ Income_Inequaliy_Brazil.R
@@ -1,3 +1,4 @@
+## Working in Progress ##
 
 ## DATA PREPARATION
 # Installing the Packages
@@ -17,13 +18,13 @@ dados_pnadc <- get_pnadc(year = 2021, quarter = 3, vars = c("VD4020", "V2007"))
 
 
 ## DATA ANALYSIS
-# Classe do objeto
+# Identifing the class of object
 class(dados_pnadc) # útil para análises de dados amostrais complexos
 
-# Obter nº total de homens e mulheres
+# Finding the number of male and female
 svytotal(x = ~V2007, design = dados_pnadc, na.rm = TRUE)
 
-# Estimar o índice de Gini
+# Estimating the Gini coefficients
 dados_pnadc %>%
   convey_prep() %>%
   svygini(formula = ~VD4020, na.rm = TRUE)
